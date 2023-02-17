7a70e41f8fe59bc746a9a8f9e164b0ff44c7fac7
Ronald#1000 <private>
2023-02-17 10:41:53 +0100

    More work...

---
+++ redm_lib/Controls.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedLib
+{
+    public struct GameControl
+    {
+        public readonly uint Hash;
+        public readonly string Name;
+        public readonly string QWERTY;
+        public readonly string GameVersion;
+
+        public GameControl(uint _Hash, string _Name, string _QWERTY = "", string _GameVersion = "")
+        {
+            this.Hash = _Hash;
+            this.Name = _Name;
+            this.QWERTY = _QWERTY;
+            this.GameVersion = _GameVersion;
+        }
+
+        public override string ToString() => $"{Name}";
+    }
+    public static class Controls
+    {
+        public static GameControl INPUT_FRONTEND_RIGHT_AXIS_X = new GameControl(0x3D23549A, "INPUT_FRONTEND_RIGHT_AXIS_X", "[", "1207.80");
+    }
+}
---
+++ redm_lib/Lib.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+
+namespace RedLib
+{
+    /// <summary>
+	/// Replaces CitizenFX.Core.Native.API;
+	/// </summary>
+    public class Lib : Natives
+    {
+        /// <summary>
+        /// Waits in x seconds.
+        /// </summary>
+        /// <param name="_sec">Seconds to wait in.</param>
+        public override void Wait(int _sec)
+        {
+            base.Wait(_sec * 1000);
+        }
+
+        /// <summary>
+		/// Calculates the magnitude of a vector.
+		/// </summary>
+        public virtual float Vmag(Vector3 _vector)
+        {
+            return base.Vmag(_vector.X, _vector.Y, _vector.Z);
+        }
+
+        /// <summary>
+		/// Calculates the magnitude of a vector but does not perform Sqrt operations. (Its way faster)
+		/// </summary>
+        public virtual float Vmag2(Vector3 _vector)
+        {
+            return base.Vmag2(_vector.X, _vector.Y, _vector.Z);
+        }
+
+        /// <summary>
+		/// Calculates distance between vectors.
+		/// </summary>
+        public virtual float Vdist(Vector3 _vector1, Vector3 _vector2)
+        {
+            return base.Vdist(_vector1.X, _vector1.Y, _vector1.Z, _vector2.X, _vector2.Y, _vector2.Z);
+        }
+
+        /// <summary>
+		/// Calculates distance between vectors but does not perform Sqrt operations. (Its way faster)
+		/// </summary>
+        public virtual float Vdist2(Vector3 _vector1, Vector3 _vector2)
+        {
+            return base.Vdist2(_vector1.X, _vector1.Y, _vector1.Z, _vector2.X, _vector2.Y, _vector2.Z);
+        }
+    }
+}
