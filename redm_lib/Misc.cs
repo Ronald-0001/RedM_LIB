09d46bc5ec439fb6787892f51ba126627c292f2e
Ronald-0001 <106472835+Ronald-0001@users.noreply.github.com>
2023-02-24 23:30:13 +0100

    Beginning of Player struct replacement

--- redm_lib/Misc.cs
+++ redm_lib/Misc.cs
@@ -6,44 +6,100 @@ using System.Threading.Tasks;
 
 namespace RedLib
 {
-    public struct SID // Server Identifier
+    /// <summary>
+    /// Replaces CitizenFX.Core.Player "CitizenFX.Core.Player is not valid with redm's dll???"
+    /// //
+    /// ServerEvent([FromSource] string source_str)
+    /// Player player = source_str;
+    /// </summary>
+    public struct Player // RedLib version of CitizenFX.Core.Player
     {
-        private readonly int sid;
-        public SID(int _id)
+        public int ServerId { get { return Source.Int; } }
+        public Source Source { get; private set; }
+        public UUID Uuid { get; private set; }
+        public Player(Source _source)
         {
-            this.sid = _id;
+            this.Source = _source;
+            this.Uuid = new UUID();
         }
-        public override string ToString() => $"{this.sid}";
+        public override string ToString() => this.Source.Int.ToString();
         //
-        public static implicit operator SID(int _sid)  // explicit int to sid conversion operator
+        public static implicit operator Player(Source _source)  // implicit Source to Player conversion operator
         {
-            return new SID(_sid); // explicit conversion
+            return new Player(_source); // implicit conversion
         }
-        public static implicit operator int(SID _sid)  // implicit sid to int conversion operator
+        public static implicit operator Player(string _source_str)  // implicit string to Player conversion operator
         {
-            return _sid.sid; // implicit conversion
+            return new Player(_source_str); // implicit conversion
+        }
+        public static implicit operator Source(Player _player)  // implicit Player to Source conversion operator
+        {
+            return _player.Source; // implicit conversion
+        }
+        public static implicit operator string(Player _player)  // implicit Player to string conversion operator
+        {
+            return _player.Source.String; // implicit conversion
+        }
+        public static implicit operator int(Player _player)  // implicit Player to int conversion operator
+        {
+            return _player.Source.Int; // implicit conversion
+        }
+        public static implicit operator UUID(Player _player)  // implicit Player to UUID conversion operator
+        {
+            return _player.Uuid; // implicit conversion
         }
     }
+    /// <summary>
+    /// ServerEvent([FromSource] string source_str)
+    /// Source source = source_str;
+    /// </summary>
+    public struct Source // Used with [FromSource] in server events
+    {
+        public string String { get; private set; }
+        public int Int { get; private set; }
+        public Source(string _source_str)
+        {
+            String = _source_str;
+            if (Int32.TryParse(_source_str.Substring(4), out int i)) Int = i;
+            else { Print.Error($"Failed to parse Source: {_source_str}"); Int = -1; }
+        }
+        public override string ToString() => this.String;
+        //
+        public static implicit operator Source(string _source_str)  // implicit string to Source conversion operator
+        {
+            return new Source(_source_str); // implicit conversion
+        }
+        public static implicit operator string(Source _source)  // implicit Source to string conversion operator
+        {
+            return _source.String; // implicit conversion
+        }
+        public static implicit operator int(Source _source)  // implicit Source to int conversion operator
+        {
+            return _source.Int; // implicit conversion
+        }
+    }
+    /// <summary>
+    /// Unique User Identifier
+    /// //
+    /// Nullable UUID Struct
+    /// </summary>
     public struct UUID // Unique User Identifier
     {
-        private readonly uint uuid;
-        public UUID(uint _uuid)
+        private readonly uint? Uint;
+        public bool Valid { get { return this.Uint.HasValue; } }
+        public UUID(uint? _uuid = null)
         {
-            this.uuid = _uuid;
+            this.Uint = _uuid;
         }
-        public override string ToString() => $"{this.uuid}";
+        public override string ToString() => this.Uint.HasValue ? this.Uint.Value.ToString() : "null";
         //
-        public static implicit operator UUID(int _uuid)  // explicit uint to uuid conversion operator
+        public static implicit operator UUID(uint _uuid)  // implicit uint to uuid conversion operator
         {
-            return new UUID((uint)_uuid); // explicit conversion
-        }
-        public static implicit operator UUID(uint _uuid)  // explicit uint to uuid conversion operator
-        {
-            return new UUID(_uuid); // explicit conversion
+            return new UUID(_uuid); // implicit conversion
         }
         public static implicit operator uint(UUID _uuid)  // implicit uuid to uint conversion operator
         {
-            return _uuid.uuid; // implicit conversion
+            return _uuid.Uint.GetValueOrDefault(0); // implicit conversion
         }
     }
 }
